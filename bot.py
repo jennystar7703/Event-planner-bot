308acd1a719101c32fce751673c31e743ca224d4
jennystar7703 <tt7703ok@gmail.com>
2024-10-08 13:16:02 +0900

    initial configurations

--- bot.py
+++ bot.py
@@ -32,9 +32,11 @@ Setup bot intents (events restrictions)
 For more information about intents, please go to the following websites:
 https://discordpy.readthedocs.io/en/latest/intents.html
 https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
+"""
 
+# Default Intents:
+intents = discord.Intents.default()
 
-Default Intents:
 intents.bans = True
 intents.dm_messages = True
 intents.dm_reactions = True
@@ -54,21 +56,11 @@ intents.typing = True
 intents.voice_states = True
 intents.webhooks = True
 
-Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
+#Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
 intents.members = True
 intents.message_content = True
 intents.presences = True
-"""
-
-intents = discord.Intents.default()
-
-"""
-Uncomment this if you want to use prefix (normal) commands.
-It is recommended to use slash commands and therefore not use prefix commands.
-
-If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
-"""
-# intents.message_content = True
+intents.message_content = True
 
 # Setup both of the loggers
 
@@ -290,4 +282,5 @@ class DiscordBot(commands.Bot):
 load_dotenv()
 
 bot = DiscordBot()
-bot.run(os.getenv("TOKEN"))
+#bot.run(os.getenv("TOKEN"))
+bot.run(config["BOT_TOKEN"])
